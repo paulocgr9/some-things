315b59dafa20ac7b953137a96e3f119ed0c89af5
PAULO CÉSAR GOMES RODRIGUES <B51979@fgv.edu.br>
2023-04-28 12:35:45 -0300

    1.6.0

--- Password/pass.py
+++ Password/pass.py
@@ -7,26 +7,29 @@ num = input("Numbers? \n(y)yes (n)no ")
 char = input("Special Characters? \n(y)yes (n)no ")
 
 p = []
-if upper == "y":
+if upper != "n" and upper == "y":
     with open('upper.txt', 'r') as u:
         up = u.readlines()
-        p = p.append(up)
-if lower == "y":
+        for i in up:
+            p.append(i)
+if lower != "n" and lower == "y":
     with open('lower.txt', 'r') as l:
         lo = l.readlines()
-        p = p.append(lo)
-if num == "y":
+        for i in lo:
+            p.append(i)
+if num != "n" and num == "y":
     with open('num.txt', 'r') as n:
         nu = n.readlines()
-        p = p.append(nu)
-if char == "y":
+        for i in nu:
+            p.append(i)
+if char != "n" and char == "y":
     with open('special.txt', 'r') as s:
         sp = s.readlines()
-        p = p.append(sp)
+        for i in sp:
+            p.append(i)
 
-final = []
+m = ""
 for i in range(characters):
-    final.append(random.choice(p).strip("\n"))
-    password = "".join(final)
-    
-print(password)
+    m = m.join(random.choice(p)).strip("\n")
+
+print(m)
