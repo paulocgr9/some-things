516b0ceb041405e0a0783a25ff8de1f9f6dfc636
PAULO CÉSAR GOMES RODRIGUES <B51979@fgv.edu.br>
2023-04-28 09:06:49 -0300

    1.0.5

--- Password/pass.py
+++ Password/pass.py
@@ -6,16 +6,27 @@ lower = input("Lowercase? \n(y)yes (n)no ")
 num = input("Numbers? \n(y)yes (n)no ")
 char = input("Special Characters? \n(y)yes (n)no ")
 
-if upper == "y" and lower =="n" and num == "n" and char == "n":
-    with open('abbb.txt', 'r') as abbb:
-        e = abbb.readlines()
-elif upper == "n" and lower =="y" and num == "n" and char == "n":
-    with open('babb.txt', 'r') as babb:
-        e = babb.readlines()
-        
-l = []
+p = []
+if upper == "y":
+    with open('upper.txt', 'r') as u:
+        up = u.readlines()
+        p = p.append(up)
+if lower == "y":
+    with open('lower.txt', 'r') as l:
+        lo = l.readlines()
+        p = p.append(lo)
+if num == "y":
+    with open('num.txt', 'r') as n:
+        nu = n.readlines()
+        p = p.append(nu)
+if char == "y":
+    with open('special.txt', 'r') as s:
+        sp = s.readlines()
+        p = p.append(sp)
+
+final = []
 for i in range(characters):
-    l.append(random.choice(e).strip("\n"))
-    password = "".join(l)
+    final.append(random.choice(p).strip("\n"))
+    password = "".join(final)
     
 print(password)
